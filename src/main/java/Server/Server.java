364bc422bcbb324a0cdec09636931df19bf497c7
Vasiliy Boynov <bot52@yandex.ru>
2021-05-14 16:50:41 +0300

    corect work procedure : autorization (Client/Server); corect work with DB;

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,5 +1,47 @@
 package Server;
 
-public final class Server {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.DefaultChannelPromise;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import java.nio.channels.FileLock;
 
+public final class Server {
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8055"));
+
+    public static void main(String[] args) throws Exception {
+        SelfSignedCertificate ssc = new SelfSignedCertificate();
+        SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())
+                .build();
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new SecureHolderServerInitializer(sslCtx));
+
+            ChannelFuture future = b.bind(PORT).sync();
+            System.out.println("Server started");
+
+
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
+
+
